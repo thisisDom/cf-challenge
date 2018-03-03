c4c3db9fc6236034228eff846782b2afabc0a490
Dominique Crowther <Dominique.Crowther@gmail.com>
2018-03-02 16:46:07 -0800

    Implement Author filtering on issues
    -Add component for rendering issue filters
    
    -Add styling for menu header
    
    -Add mixin for input placeholder styling
    
    -Add green to color palette

--- src/components/issues/Issues.js
+++ src/components/issues/Issues.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import IssueContainer from './Issue/Issue';
+import IssueContainer from './issue/Issue';
+import MenuOption from './menu-option/MenuOption';
 import './Issues.css';
 
 class Issues extends Component {
@@ -8,8 +9,24 @@ class Issues extends Component {
     super(props);
     this.state = {
       issues: [],
+      issueAuthors: [],
+      showAuthors: false,
+      authorFilter: null,
     };
   }
+   applyAuthorFilter(username){
+     if (username === this.state.authorFilter){
+       this.setState({
+         authorFilter: null
+       })
+     }
+     else{
+       this.setState({
+         authorFilter: username
+       });
+     }
+   }
+
   componentWillMount(){
     axios.get('https://api.github.com/repos/DestinyItemManager/DIM/issues', {
       params: {
@@ -17,30 +34,69 @@ class Issues extends Component {
       }
     })
     .then(response => {
+      let authors = {}
+      let issues = response.data.map(issue => {
+        authors[issue.user.login] = {
+          username: issue.user.login,
+          avatar_url: issue.user.avatar_url,
+        };
+        return issue;
+      });
+      let issueAuthors = [];
+      for (let user in authors){
+        issueAuthors.push(authors[user]);
+      }
       this.setState({
-        issues: response.data,
+        issues: issues,
+        issueAuthors: issueAuthors,
       });
     });
   }
 
+  toggleMenu(type){
+    switch (type){
+      case "authors":
+      this.setState({
+        showAuthors: !this.state.showAuthors,
+      });
+      return;
+      default:
+      return;
+    }
+  }
   renderIssues(){
     let issues = this.state.issues;
-    return issues.map(issue => ( <IssueContainer key={issue.number} data={issue} /> )) ;
+
+    if (this.state.authorFilter){
+      issues = issues.filter(issue => issue.user.login === this.state.authorFilter );
+    }
+
+
+    return issues.map(issue => ( <IssueContainer key={issue.number} data={issue} /> ));
   }
 
   render(){
-    const issues = this.renderIssues();
+    const { issueAuthors, showAuthors } = this.state
     return (
     <div className="issues-container">
       <div className="issues-header">
         <div className="issues-breakdown-container">
-          --PLACEHOLDER--
         </div>
-        <div className="issues-sort-container">
+        <div className="issues-menu-container">
+          <ul>
+             <MenuOption
+                type="author"
+                title="Author"
+                options={issueAuthors}
+                toggleMenu={() => this.toggleMenu("authors")}
+                optionOnClick={(username) => this.applyAuthorFilter(username)}
+                showMenu={showAuthors}
+             />
+           </ul>
         </div>
       </div>
       <ul className="issues-list-container">
-      {issues}
+      {this.renderIssues()}
       </ul>
     </div>
   )}
---
+++ src/components/issues/menu-option/MenuOption.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './MenuOption.css';
+
+class MenuOption extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      showMenu: this.props.showMenu,
+    }
+  }
+  componentWillReceiveProps(newProps){
+    this.setState({
+      showMenu: newProps.showMenu
+    });
+  }
+
+  showMenu(){
+    if(this.state.showMenu){
+      let title = this.props.title;
+      return (
+        <div className="menu-dropdown-wrapper">
+          <div className="menu-dropdown-modal">
+            <div className="menu-dropdown-container">
+              <div className="menu-dropdown-header">
+                <span>Filter by {title.toLowerCase()}</span>
+                <svg className="menu-dropdown-close menu-close" height="16" role="img" version="1.1" viewBox="0 0 12 16" width="12" onClick={() => this.props.toggleMenu()}>
+                  <path fillRule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"></path>
+                </svg>
+              </div>
+              <ul className="menu-options-list">
+                {this.renderListItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+  renderListItems(){
+    switch(this.props.type){
+      case 'author':
+        return this.props.options.map(author => {
+          return (
+            <li key={author.username} onClick={() => this.props.optionOnClick(author.username)}>
+              <img className="menu-item-avatar" src={author.avatar_url} alt="G" />
+              <span>{author.username}</span>
+            </li>
+          )
+        })
+      default:
+        return;
+    }
+  }
+  render(){
+    const { toggleMenu, title } = this.props;
+    return (
+      <li className="menu-option-container">
+        <div className="menu-title" onClick={() => toggleMenu()}>{title}</div>
+        {this.showMenu()}
+      </li>
+  )}
+}
+
+export default MenuOption
